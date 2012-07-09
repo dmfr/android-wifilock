582f683ff794daac9daa5d476775ed33dca03cbd
Damien Mirand <dm@mirabel-sil.com>
2012-07-09 02:48:04 +0200

    Add UI to set prefs

--- src/za/dams/wifilock/ConfigActivity.java
+++ src/za/dams/wifilock/ConfigActivity.java
@@ -1,63 +1,88 @@
 package za.dams.wifilock;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class ConfigActivity extends Activity {
+public class ConfigActivity extends PreferenceActivity 
+	implements SharedPreferences.OnSharedPreferenceChangeListener {
 	
 	private static final String TAG = "ConfigActivity";
+	
+	private ConfigController mCfgCtrl ;
 
     
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_config);
         
-        // Log.w(TAG,"Start activity");
-        /*
-        Intent intent = getApplication().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean isPlugged = ( plugged == BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB );
+        PreferenceManager.setDefaultValues(this, R.xml.config, false);
+        addPreferencesFromResource(R.xml.config);
         
-        String str ;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        prefs.registerOnSharedPreferenceChangeListener(this) ;
         
-        if( isPlugged ) {
-        	str = "Is plugged" ;
-        }
-        else {
-        	str = "Not Plugged" ;
-        }
+        mCfgCtrl = new ConfigController(this) ;
         
-        Toast.makeText(getApplication(), str, Toast.LENGTH_LONG).show();*/
-        
+        // Launch service if not running
         if( !MyService.isRunning(getApplication()) ) {
-        	Log.w(TAG,"Lauching service") ;
         	startService(new Intent(this, MyService.class)) ;
         }
-        else {
-        	Log.w(TAG,"Service already running...") ;
-        }
-        	
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_config, menu);
-        return true;
     }
 
     
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onSharedPreferenceChanged(SharedPreferences pref, String label) {
+    	if( MyService.isRunning(getApplication()) ) {
+    		stopService(new Intent(this, MyService.class)) ;
+    	}
+    	startService(new Intent(this, MyService.class)) ;
     }
+    
+    
+    private class ConfigController implements Preference.OnPreferenceClickListener {
+    	
+    	private CheckBoxPreference chkHighPerf ;
+    	private CheckBoxPreference chkWifiLock ;
+    	private CheckBoxPreference chkWifiPingKA ;
+    	
+    	@SuppressWarnings("deprecation")
+		public ConfigController( ConfigActivity configActivity ) {
+    		chkHighPerf = (CheckBoxPreference) configActivity.findPreference("chkHighPerf") ;
+    		chkHighPerf.setOnPreferenceClickListener(this) ;
+    		chkWifiLock = (CheckBoxPreference) configActivity.findPreference("chkWifiLock") ;
+    		chkWifiLock.setOnPreferenceClickListener(this) ;
+    		chkWifiPingKA = (CheckBoxPreference) configActivity.findPreference("chkWifiPingKA") ;
+    		chkWifiPingKA.setOnPreferenceClickListener(this) ;
+    		
+    		setDisableStates() ;
+    	}
+
+		public boolean onPreferenceClick(Preference pref) {
+			if( pref.getKey().equals("chkWifiLock") ) {
+				setDisableStates() ;
+			}
+			return false;
+		}
+		
+		
+		private void setDisableStates() {
+			if( chkWifiLock != null && chkWifiPingKA != null ) {
+				if( chkWifiLock.isChecked() ) {
+					chkWifiPingKA.setEnabled(true) ;
+ 				}
+				else{
+					chkWifiPingKA.setEnabled(false) ;
+				}
+				
+			}
+ 		}
+    	
+     }
 
 }
